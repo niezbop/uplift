6231ce3197699da86e907ea1f194e1a234d14b42
Paul Niezborala <paulniezbo@gmail.com>
2018-03-27 14:50:49 +0200

    Create basic Editor fields for InstallSpecPath and DependencyDefinition

---
+++ Assets/Plugins/Editor/Uplift/Common/UpliftGUILayout.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using Uplift.Schemas;
+
+namespace Uplift.Common.Editor
+{
+    public class UpliftGUILayout
+    {
+        public static DependencyDefinition DependencyField(string label, DependencyDefinition def)
+        {
+            DependencyDefinition result = new DependencyDefinition()
+            {
+                Name = def.Name,
+                Version = def.Version
+            };
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(label);
+            result.Name = EditorGUILayout.TextField(result.Name);
+            result.Version = EditorGUILayout.TextField(result.Version);
+            EditorGUILayout.EndHorizontal();
+
+            return result;
+        }
+
+        public static InstallSpecPath InstallSpecPathField(string label, InstallSpecPath spec)
+        {
+            InstallSpecPath result = new InstallSpecPath
+            {
+                Path = spec.Path,
+                Type = spec.Type
+            };
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(label);
+            result.Type = (InstallSpecType)EditorGUILayout.EnumPopup(result.Type);
+            result.Path = EditorGUILayout.TextField(result.Path);
+            EditorGUILayout.EndHorizontal();
+
+            return result;
+        }
+    }
+}
